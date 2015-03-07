506b098c2ff507e4a66ee4ae2361f6ae2e48e1e1
shivendrasrivastava <mail2shivendra@gmail.com>
2015-03-07 00:31:44 -0600

    Committing changes made to f1 data retrieval mechanism.

--- f1drivers.py
+++ f1drivers.py
@@ -4,44 +4,48 @@ import json
 import pymongo
 
 
-def read_data():
-    url = 'http://ergast.com/api/f1/2010/driverStandings.json'
+def read_data(url, year):
     r = requests.get(url)
-    print r.headers.get('content-type')
     data = json.loads(r.text)
-    print type(data)
-    # print data["MRData"]["StandingsTable"]["StandingsLists"]
     posts = connect_mongo()
     print "Details are as follows :-"
     for standings_list in data["MRData"]["StandingsTable"]["StandingsLists"]:
-        # print json.dumps(standings_list["DriverStandings"], indent=4)
         for drivers_standings in standings_list["DriverStandings"]:
             document = {}
             print json.dumps(drivers_standings, indent=4)
             constructor = drivers_standings["Constructors"][0]
-            document["const"] = constructor
-            document["driver"] = drivers_standings["Driver"]
+            document["constructor"] = remove_data(constructor)
+            document["driver"] = remove_data(drivers_standings["Driver"])
+            document["wins"] = drivers_standings["wins"]
+            document["points"] = drivers_standings["points"]
+            document["position"] = drivers_standings["position"]
+            document["year"] = year
             json_doc = json.dumps(document)
             print json_doc
             posts.insert(document)
-
-    # formatted_data = json.dumps(data, sort_keys=True, indent=4)
-
-    # print formatted_data
 
 
 def connect_mongo():
     client = pymongo.MongoClient('localhost', 27017)
     db = client.f1db
-    print "Database names are "
-    print client.database_names()
-    print "Collections in f1db are "
-    print db.collection_names()
     posts = db.test
-    print "Record in f1db.test are "
-    print posts.find_one()
     return posts
 
 
-read_data()
-connect_mongo()
+def remove_data(data):
+    data.pop('url', None)
+    return data
+
+
+def start():
+    url_part_1 = "http://ergast.com/api/f1/"
+    url_part_2 = 2010
+    url_part_3 = "/driverStandings.json"
+
+    for x in range(0, 4):
+        year = str(url_part_2+x)
+        url = url_part_1 + year + url_part_3
+        print url
+        read_data(url, year)
+
+start()
